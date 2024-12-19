2ce26cc115034446b5c6057ba6ce2ae27f1dc6d6
vipins <https://gitlab.intoday.in>
2024-12-19 23:27:36 +0530

    config update

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,9 @@
 export default {
     //LOCAL URL's
-    BASE_URL: "http://localhost:3008",
-    SERVER_BASE_URL: "http://localhost:3008",
+    // BASE_URL: "http://localhost:3008",
+    // SERVER_BASE_URL: "http://localhost:3008",
+    BASE_URL: "https://ecoms-store.netlify.app",
+    SERVER_BASE_URL: "https://ecoms-store.netlify.app",
     API_COSM_URL: "",
     RESOURCE_PATH: "https://cdn.jsdelivr.net/npm/swiper@11",
     RES_VER:123
